71ba8c7d66b57ef046a8f9cfef26948e9a66e5a7
Cristina Fontanet <cristina.fontanet@est.fib.upc.edu>
2015-11-26 00:05:53 +0100

    initial BD + initialController

--- app/src/main/java/com/example/cristinafontanet/parkingidi/MainActivity.java
+++ app/src/main/java/com/example/cristinafontanet/parkingidi/MainActivity.java
@@ -4,16 +4,75 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
 
+    ImageView p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15;
+
+    Controller bigControl;
+
+    public static final int SHORT = 3;
+
+   // protected DateFormat();
+
+    private void iniButtons() {
+        p1 = (ImageView)findViewById(R.id.p1);
+        p1.setOnClickListener(this);
+
+        p2 = (ImageView)findViewById(R.id.p2);
+        p2.setOnClickListener(this);
+
+        p3 = (ImageView)findViewById(R.id.p3);
+        p3.setOnClickListener(this);
+
+        p4 = (ImageView)findViewById(R.id.p4);
+        p4.setOnClickListener(this);
+
+        p5 = (ImageView)findViewById(R.id.p5);
+        p5.setOnClickListener(this);
+
+        p6 = (ImageView)findViewById(R.id.p6);
+        p6.setOnClickListener(this);
+
+        p7 = (ImageView)findViewById(R.id.p7);
+        p7.setOnClickListener(this);
+
+        p8 = (ImageView)findViewById(R.id.p8);
+        p8.setOnClickListener(this);
+
+        p9 = (ImageView)findViewById(R.id.p9);
+        p9.setOnClickListener(this);
+
+        p10 = (ImageView)findViewById(R.id.p10);
+        p10.setOnClickListener(this);
+
+        p11 = (ImageView)findViewById(R.id.p11);
+        p11.setOnClickListener(this);
+
+        p12 = (ImageView)findViewById(R.id.p12);
+        p12.setOnClickListener(this);
+
+        p13 = (ImageView)findViewById(R.id.p13);
+        p13.setOnClickListener(this);
+
+        p14 = (ImageView)findViewById(R.id.p14);
+        p14.setOnClickListener(this);
+
+        p15 = (ImageView)findViewById(R.id.p15);
+        p15.setOnClickListener(this);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        bigControl = new Controller(this);
+        bigControl.estatPakingBD();
+        iniButtons();
 
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,5 +94,10 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
